ecae4e9c69916b06a40b215457c55e25c8c34d96
Alexandros Schillings <aschillings@gmail.com>
2013-03-10 15:13:17 +0000

    Added copyright info. Made the Colourised list a bit more intuitive.

--- ReflectiveDrawableLoader/src/co/uk/alt236/reflectivedrawableloader/containers/DrawableResourceContainer.java
+++ ReflectiveDrawableLoader/src/co/uk/alt236/reflectivedrawableloader/containers/DrawableResourceContainer.java
@@ -1,5 +1,22 @@
+/*******************************************************************************
+ * Copyright 2013 Alexandros Schillings
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package co.uk.alt236.reflectivedrawableloader.containers;
 
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.PorterDuff.Mode;
 import android.widget.ImageView;
 
@@ -7,11 +24,12 @@ public class DrawableResourceContainer {
     private final int mResourceId;
     private final Integer mColourFilterColour;
     private final String mDrawableName;
-    
+    private ColorFilter mColourFilter;
+
     public DrawableResourceContainer(int resourceId, Integer colourFilterColour) {
 	this(null, resourceId, colourFilterColour);
     }
-    
+
     public DrawableResourceContainer(String drawableName, int resourceId, Integer colourFilterColour) {
 	this.mResourceId = resourceId;
 	this.mColourFilterColour = colourFilterColour;
@@ -23,7 +41,23 @@ public class DrawableResourceContainer {
     }
 
     public String getDrawableName() {
-        return mDrawableName;
+	return mDrawableName;
+    }
+
+    private ColorFilter getOverridingColorFilter(int color){
+	if(mColourFilter == null){
+	    int red = (color & 0xFF0000) / 0xFFFF;
+	    int green = (color & 0xFF00) / 0xFF;
+	    int blue = color & 0xFF;
+
+	    float[] matrix = { 0, 0, 0, 0, red
+		    , 0, 0, 0, 0, green
+		    , 0, 0, 0, 0, blue
+		    , 0, 0, 0, 1, 0 };
+
+	    mColourFilter =  new ColorMatrixColorFilter(matrix);
+	}
+	return mColourFilter;
     }
 
     public int getResourceId() {
@@ -34,6 +68,16 @@ public class DrawableResourceContainer {
 	return !(mColourFilterColour == null);
     }
 
+
+    public void setDrawableWithColorOverrideMatrix(ImageView iv) {
+	iv.setImageResource(mResourceId);
+	if (hasColourFilter()) {
+	    iv.setColorFilter(getOverridingColorFilter(mColourFilterColour));
+	} else {
+	    iv.setColorFilter(null);
+	}
+    }
+
     public void setDrawableWithPorterDuffMultiply(ImageView iv) {
 	iv.setImageResource(mResourceId);
 	if (hasColourFilter()) {
@@ -42,5 +86,4 @@ public class DrawableResourceContainer {
 	    iv.setColorFilter(null);
 	}
     }
-
 }
--- ReflectiveDrawableLoaderSampleApp/src/co/uk/alt236/reflectivedrawableloader/sampleapp/util/ColorisedDrawableArrayAdapter.java
+++ ReflectiveDrawableLoaderSampleApp/src/co/uk/alt236/reflectivedrawableloader/sampleapp/util/ColorisedDrawableArrayAdapter.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2013 alex
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package co.uk.alt236.reflectivedrawableloader.sampleapp.util;
 
 
@@ -15,18 +30,20 @@ import co.uk.alt236.reflectivedrawableloader.containers.DrawableResourceContaine
 import co.uk.alt236.reflectivedrawableloader.sampleapp.R;
 
 public class ColorisedDrawableArrayAdapter extends ArrayAdapter<DrawableResourceContainer>{
-    final static int mLayout = R.layout.list_item_icon_check;
+    final static int mLayout = R.layout.list_item_icon_check_colorised;
     final static int mMissingIconId = R.drawable.ic_missing_icon;
     
     final Context mContext;
     final List<DrawableResourceContainer> mItemList;
     final ReflectiveDrawableLoader mReflectiveLoader;
+    final int mColourTransparent;
     
     public ColorisedDrawableArrayAdapter(Context context, List<DrawableResourceContainer> itemList) {
         super(context, mLayout);
         mReflectiveLoader = ReflectiveDrawableLoader.getInstance(context);
         mContext = context;
         mItemList = itemList;
+        mColourTransparent = context.getResources().getColor(android.R.color.transparent);
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -62,8 +79,12 @@ public class ColorisedDrawableArrayAdapter extends ArrayAdapter<DrawableResource
 
         private TextView name = null;
         private ImageView image1 = null;
+        private ImageView image2 = null;
+        private ImageView image3 = null;
+        private ImageView image4 = null;
 
         private View row = null;
+
 
         public Wrapper(View row) {
             this.row = row;
@@ -76,6 +97,27 @@ public class ColorisedDrawableArrayAdapter extends ArrayAdapter<DrawableResource
             return(image1);
         }
 
+        public ImageView getImage2() {
+            if (image2 == null) {
+                image2 = (ImageView) row.findViewById(R.id.image2);
+            }
+            return(image2);
+        }
+
+        public ImageView getImage3() {
+            if (image3 == null) {
+                image3 = (ImageView) row.findViewById(R.id.image3);
+            }
+            return(image3);
+        }
+
+        public ImageView getImage4() {
+            if (image4 == null) {
+                image4 = (ImageView) row.findViewById(R.id.image4);
+            }
+            return(image4);
+        }
+
         public TextView getName() {
             if (name == null) {
                 name = (TextView) row.findViewById(R.id.name);
@@ -83,10 +125,26 @@ public class ColorisedDrawableArrayAdapter extends ArrayAdapter<DrawableResource
             return(name);
         }
 
-        public void populateFrom(DrawableResourceContainer drawableContainer) {
-            if (drawableContainer != null) {        	
-                getName().setText(drawableContainer.getDrawableName());
-                drawableContainer.setDrawableWithPorterDuffMultiply(getImage1());
+        public void populateFrom(DrawableResourceContainer container) {
+            if (container != null) {        	
+                getName().setText(container.getDrawableName());
+                
+                // This is the original Drawable
+                getImage1().setImageResource(container.getResourceId());
+                
+                // This is the colour we are using:
+                if(container.getColourFilterColour() == null){
+                    getImage2().setBackgroundColor(mColourTransparent);
+                } else {
+                    getImage2().setBackgroundColor(container.getColourFilterColour());
+                }
+                
+                // This is applied using PorterDuff Mode Multiply
+                container.setDrawableWithPorterDuffMultiply(getImage3());
+                
+                // This is applied using a custom matrix which will replace
+                // other colours with the colourfilter one.
+                container.setDrawableWithColorOverrideMatrix(getImage4());
             }
         }
 
